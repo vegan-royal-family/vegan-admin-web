a7a3fc8ec194a5a661eb576152323b77e6ee4bea
suhotang <dkdudkc22@gmail.com>
2022-09-24 17:24:31 +0900

    feat(typography): add styles

---
+++ styles/typography.js
@@ -0,0 +1,38 @@
+import { css } from "@emotion/react";
+
+export const typography = {
+  heading1: css`
+    font-weight: 400;
+    font-size: 3.5rem;
+    line-height: 3.5rem;
+  `,
+  heading2: css`
+    font-weight: 400;
+    font-size: 3rem;
+    line-height: 3rem;
+  `,
+  heading3: css`
+    font-weight: 400;
+    font-size: 2.25rem;
+    line-height: 2.5rem;
+  `,
+  heading4: css`
+    font-weight: 400;
+    font-size: 1.875rem;
+    line-height: 2.25rem;
+  `,
+  heading5: css`
+    font-weight: 400;
+    font-size: 1.5rem;
+    line-height: 2rem;
+  `,
+  heading6: css`
+    font-weight: 400;
+    font-size: 1.25rem;
+    line-height: 1.75rem;
+  `,
+  body1: css`
+    font-size: 1.125rem;
+    line-height: 1.75rem;
+  `,
+};
