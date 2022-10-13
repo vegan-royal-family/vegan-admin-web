b457251f76606e21a2903ee9885375a407f2cb3a
hyojin <injulme0309@naver.com>
2022-10-13 22:44:39 +0900

    아이콘 컴포넌트 구현. 스토리 작성

---
+++ components/common/Icon.js
@@ -0,0 +1,138 @@
+import PropTypes from "prop-types";
+
+import AddOne from "../../assets/icon/add-one.svg";
+import Apple from "../../assets/icon/apple.svg";
+import AttentionFilled from "../../assets/icon/attention-filled.svg";
+import Attention from "../../assets/icon/attention.svg";
+import CheckOne from "../../assets/icon/check_one.svg";
+import CheckOneFilled from "../../assets/icon/check-one-filled.svg";
+import CircleWhite from "../../assets/icon/circle-white.svg";
+import Circle from "../../assets/icon/circle.svg";
+import CloseOneFilled from "../../assets/icon/close-one-filled.svg";
+import CloseOne from "../../assets/icon/close-one.svg";
+import CloseSmall from "../../assets/icon/close-small.svg";
+import Remove from "../../assets/icon/delete.svg";
+import DoubleLeft from "../../assets/icon/double_left.svg";
+import DoubleRight from "../../assets/icon/double_right.svg";
+import Down from "../../assets/icon/down.svg";
+import Google from "../../assets/icon/google.svg";
+import Home from "../../assets/icon/home.svg";
+import Kakao from "../../assets/icon/kakao.svg";
+import LeftSmall from "../../assets/icon/left_small.svg";
+import Left from "../../assets/icon/left.svg";
+import Like from "../../assets/icon/like.svg";
+import Naver from "../../assets/icon/naver.svg";
+import PhoneTelephone from "../../assets/icon/phone_telephone.svg";
+import PlayCycle from "../../assets/icon/play_cycle.svg";
+import Plus from "../../assets/icon/plus.svg";
+import PreviewCloseOne from "../../assets/icon/preview_close_one.svg";
+import PreviewClose from "../../assets/icon/preview_close.svg";
+import PreviewOpen from "../../assets/icon/preview_open.svg";
+import Redo from "../../assets/icon/redo.svg";
+import Right from "../../assets/icon/right.svg";
+import SettingConfig from "../../assets/icon/setting_config.svg";
+import SettingTwo from "../../assets/icon/setting_two.svg";
+import StarFilledHalf from "../../assets/icon/star_filled_half.svg";
+import StarFilled from "../../assets/icon/star_filled.svg";
+import Star from "../../assets/icon/star.svg";
+import Up from "../../assets/icon/up.svg";
+
+export default function Icon({ icon, size = "md", fill }) {
+  const SIZES = {
+    xxs: 12,
+    xs: 16,
+    sm: 20,
+    md: 24,
+    lg: 32,
+    xl: 40,
+  };
+
+  const args = {
+    width: SIZES[size],
+    height: SIZES[size],
+    fill,
+  };
+
+  switch (icon) {
+    case "add-one":
+      return <AddOne {...args} />;
+    case "apple":
+      return <Apple {...args} />;
+    case "attention-filled":
+      return <AttentionFilled {...args} />;
+    case "attention":
+      return <Attention {...args} />;
+    case "check-one":
+      return <CheckOne {...args} />;
+    case "check-one-filled":
+      return <CheckOneFilled {...args} />;
+    case "circle-white":
+      return <CircleWhite {...args} />;
+    case "circle":
+      return <Circle {...args} />;
+    case "close-one-filled":
+      return <CloseOneFilled {...args} />;
+    case "close-one":
+      return <CloseOne {...args} />;
+    case "close-small":
+      return <CloseSmall {...args} />;
+    case "delete":
+      return <Remove {...args} />;
+    case "double-left":
+      return <DoubleLeft {...args} />;
+    case "double-right":
+      return <DoubleRight {...args} />;
+    case "down":
+      return <Down {...args} />;
+    case "google":
+      return <Google {...args} />;
+    case "home":
+      return <Home {...args} />;
+    case "kakao":
+      return <Kakao {...args} />;
+    case "left-small":
+      return <LeftSmall {...args} />;
+    case "left":
+      return <Left {...args} />;
+    case "like":
+      return <Like {...args} />;
+    case "naver":
+      return <Naver {...args} />;
+    case "phone-telephone":
+      return <PhoneTelephone {...args} />;
+    case "play-cycle":
+      return <PlayCycle {...args} />;
+    case "plus":
+      return <Plus {...args} />;
+    case "preview-close-one":
+      return <PreviewCloseOne {...args} />;
+    case "preview-close":
+      return <PreviewClose {...args} />;
+    case "preview-open":
+      return <PreviewOpen {...args} />;
+    case "redo":
+      return <Redo {...args} />;
+    case "right":
+      return <Right {...args} />;
+    case "setting-config":
+      return <SettingConfig {...args} />;
+    case "setting-two":
+      return <SettingTwo {...args} />;
+    case "star-filled-half":
+      return <StarFilledHalf {...args} />;
+    case "star-filled":
+      return <StarFilled {...args} />;
+    case "star":
+      return <Star {...args} />;
+    case "up":
+      return <Up {...args} />;
+
+    default:
+      return;
+  }
+}
+
+Icon.propTypes = {
+  icon: PropTypes.string,
+  size: PropTypes.oneOf(["xxs", "xs", "sm", "md", "lg", "xl"]),
+};
---
+++ stories/icon.stories.js
@@ -0,0 +1,191 @@
+import Icon from "../components/common/Icon";
+
+export default {
+  title: "components/Icon",
+  component: Icon,
+};
+
+const Template = (args) => {
+  return <Icon {...args} />;
+};
+
+export const AddOne = Template.bind({});
+AddOne.args = {
+  icon: "add-one",
+  size: "md",
+};
+export const Apple = Template.bind({});
+Apple.args = {
+  icon: "apple",
+  size: "md",
+};
+export const AttentionFilled = Template.bind({});
+AttentionFilled.args = {
+  icon: "attention-filled",
+  size: "md",
+};
+export const Attention = Template.bind({});
+Attention.args = {
+  icon: "attention",
+  size: "md",
+};
+export const CheckOne = Template.bind({});
+CheckOne.args = {
+  icon: "check-one",
+  size: "md",
+};
+export const CheckOneFilled = Template.bind({});
+CheckOneFilled.args = {
+  icon: "check-one-filled",
+  size: "md",
+};
+export const CircleWhite = Template.bind({});
+CircleWhite.args = {
+  icon: "circle-white",
+  size: "md",
+};
+export const Circle = Template.bind({});
+Circle.args = {
+  icon: "circle",
+  size: "md",
+};
+export const CloseOneFilled = Template.bind({});
+CloseOneFilled.args = {
+  icon: "close-one-filled",
+  size: "md",
+};
+export const CloseOne = Template.bind({});
+CloseOne.args = {
+  icon: "close-one",
+  size: "md",
+};
+export const CloseSmall = Template.bind({});
+CloseSmall.args = {
+  icon: "close-small",
+  size: "md",
+};
+export const Remove = Template.bind({});
+Remove.args = {
+  icon: "delete",
+  size: "md",
+};
+export const DoubleLeft = Template.bind({});
+DoubleLeft.args = {
+  icon: "double-left",
+  size: "md",
+};
+export const DoubleRight = Template.bind({});
+DoubleRight.args = {
+  icon: "double-right",
+  size: "md",
+};
+export const Down = Template.bind({});
+Down.args = {
+  icon: "down",
+  size: "md",
+};
+export const Google = Template.bind({});
+Google.args = {
+  icon: "google",
+  size: "md",
+};
+export const Home = Template.bind({});
+Home.args = {
+  icon: "home",
+  size: "md",
+};
+export const Kakao = Template.bind({});
+Kakao.args = {
+  icon: "kakao",
+  size: "md",
+};
+export const LeftSmall = Template.bind({});
+LeftSmall.args = {
+  icon: "left-small",
+  size: "md",
+};
+export const Left = Template.bind({});
+Left.args = {
+  icon: "left",
+  size: "md",
+};
+export const Like = Template.bind({});
+Like.args = {
+  icon: "like",
+  size: "md",
+};
+export const Naver = Template.bind({});
+Naver.args = {
+  icon: "naver",
+  size: "md",
+};
+export const PhoneTelephone = Template.bind({});
+PhoneTelephone.args = {
+  icon: "phone-telephone",
+  size: "md",
+};
+export const PlayCycle = Template.bind({});
+PlayCycle.args = {
+  icon: "play-cycle",
+  size: "md",
+};
+export const Plus = Template.bind({});
+Plus.args = {
+  icon: "plus",
+  size: "md",
+};
+export const PreviewCloseOne = Template.bind({});
+PreviewCloseOne.args = {
+  icon: "preview-close-one",
+  size: "md",
+};
+export const PreviewClose = Template.bind({});
+PreviewClose.args = {
+  icon: "preview-close",
+  size: "md",
+};
+export const PreviewOpen = Template.bind({});
+PreviewOpen.args = {
+  icon: "preview-open",
+  size: "md",
+};
+export const Redo = Template.bind({});
+Redo.args = {
+  icon: "redo",
+  size: "md",
+};
+export const Right = Template.bind({});
+Right.args = {
+  icon: "right",
+  size: "md",
+};
+export const SettingConfig = Template.bind({});
+SettingConfig.args = {
+  icon: "setting-config",
+  size: "md",
+};
+export const SettingTwo = Template.bind({});
+SettingTwo.args = {
+  icon: "setting-two",
+  size: "md",
+};
+export const StarFilledHalf = Template.bind({});
+StarFilledHalf.args = {
+  icon: "star-filled-half",
+  size: "md",
+};
+export const StarFilled = Template.bind({});
+StarFilled.args = {
+  icon: "star-filled",
+  size: "md",
+};
+export const Star = Template.bind({});
+Star.args = {
+  icon: "star",
+  size: "md",
+};
+export const Up = Template.bind({});
+Up.args = {
+  icon: "up",
+  size: "md",
+};
