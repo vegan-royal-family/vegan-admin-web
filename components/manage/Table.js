3f8cd1f7e2d1eb7d7273bad43c1d2fcb3a5ad61e
suhotang <dkdudkc22@gmail.com>
2022-12-10 16:23:48 +0900

    Feat(테이블)   - 테이블 컴포넌트 정리

--- components/manage/Table.js
+++ components/manage/Table.js
@@ -1,136 +1,7 @@
 import React from "react";
 import styled from "@emotion/styled";
 import { useRowSelect, usePagination, useTable } from "react-table";
-import range from "utils/range.js";
-
-const defaultPageGroupCount = 4;
-
-const Pagination = (props) => {
-  const {
-    gotoPage,
-    pageCount,
-    pageIndex,
-    canPreviousPage,
-    previousPage,
-    canNextPage,
-    nextPage,
-  } = props;
-
-  const isArrangeView = pageCount <= defaultPageGroupCount + 3;
-  const lastPageIndex = pageCount - 1;
-  const prevPageIndexList = [];
-  const nextPageIndexList = [];
-
-  if (pageIndex < defaultPageGroupCount) {
-    const maxIndex = defaultPageGroupCount;
-    const minIndex = 1;
-    for (let prevIndex = pageIndex - 1; minIndex <= prevIndex; prevIndex--) {
-      prevPageIndexList.unshift(prevIndex);
-    }
-    for (let nextIndex = pageIndex + 1; nextIndex <= maxIndex; nextIndex++) {
-      nextPageIndexList.push(nextIndex);
-    }
-  } else if (pageIndex > lastPageIndex - defaultPageGroupCount) {
-    const maxIndex = lastPageIndex - 1;
-    const minIndex = lastPageIndex - defaultPageGroupCount;
-    for (let prevIndex = pageIndex - 1; minIndex <= prevIndex; prevIndex--) {
-      prevPageIndexList.unshift(prevIndex);
-    }
-    for (let nextIndex = pageIndex + 1; nextIndex <= maxIndex; nextIndex++) {
-      nextPageIndexList.push(nextIndex);
-    }
-  } else {
-    prevPageIndexList.push(pageIndex - 1);
-    nextPageIndexList.push(pageIndex + 1);
-  }
-
-  return (
-    <>
-      <button
-        className="arrowBtn"
-        onClick={() => previousPage()}
-        disabled={!canPreviousPage}
-      >
-        {"<"}
-      </button>
-      {isArrangeView ? (
-        <div className="indexBtnGroup">
-          {range(pageCount).map((index) => {
-            const selectedStyle =
-              pageIndex === index ? "selectedPageIndex" : "";
-            return (
-              <button
-                key={index}
-                className={`indexItem ${selectedStyle}`}
-                onClick={() => gotoPage(index)}
-              >
-                {index + 1}
-              </button>
-            );
-          })}
-        </div>
-      ) : (
-        <div className="indexBtnGroup">
-          {pageIndex === 0 ? (
-            <div className="indexItem selectedPageIndex">1</div>
-          ) : (
-            <button className="indexItem" onClick={() => gotoPage(0)}>
-              1
-            </button>
-          )}
-          {pageIndex >= defaultPageGroupCount && (
-            <span style={{ padding: "0 4px" }}>...</span>
-          )}
-          {prevPageIndexList.map((index) => {
-            return (
-              <button
-                key={index}
-                className="indexItem"
-                onClick={() => gotoPage(index)}
-              >
-                {index + 1}
-              </button>
-            );
-          })}
-          {pageIndex > 0 && pageIndex < lastPageIndex && (
-            <div className="indexItem selectedPageIndex">{pageIndex + 1}</div>
-          )}
-          {nextPageIndexList.map((index) => {
-            return (
-              <button
-                key={index}
-                className="indexItem"
-                onClick={() => gotoPage(index)}
-              >
-                {index + 1}
-              </button>
-            );
-          })}
-          {pageIndex < pageCount - defaultPageGroupCount && (
-            <span style={{ padding: "0 4px" }}>...</span>
-          )}
-          {pageIndex === pageCount - 1 ? (
-            <div className="indexItem selectedPageIndex">{pageCount}</div>
-          ) : (
-            <button
-              className="indexItem"
-              onClick={() => gotoPage(pageCount - 1)}
-            >
-              {pageCount}
-            </button>
-          )}
-        </div>
-      )}
-      <button
-        className="arrowBtn"
-        onClick={() => nextPage()}
-        disabled={!canNextPage}
-      >
-        {">"}
-      </button>
-    </>
-  );
-};
+import Pagination from "./Pagination";
 
 export default function Table({
   columns,
@@ -185,7 +56,6 @@ export default function Table({
     }
   );
 
-  // Render the UI for your table
   return (
     <TableStyles>
       <div className="tableWrap">
@@ -240,48 +110,20 @@ export default function Table({
         </table>
       </div>
       {!disablePagination && pageCount > 1 && (
-        <div className="pagination">
-          <div style={{ flex: 1 }}>
-            <span style={{ paddingRight: 16 }}>
-              Page {pageIndex + 1} of {pageOptions.length}
-            </span>
-            <select
-              value={pageSize}
-              onChange={(e) => {
-                setPageSize(Number(e.target.value));
-              }}
-            >
-              {[10, 20, 30, 40, 50].map((pageSize) => (
-                <option key={pageSize} value={pageSize}>
-                  Show {pageSize}
-                </option>
-              ))}
-            </select>
-          </div>
-          <Pagination
-            {...{
-              gotoPage,
-              previousPage,
-              nextPage,
-              canPreviousPage,
-              canNextPage,
-              pageCount,
-              pageIndex,
-            }}
-          />
-          <span style={{ marginLeft: 16 }}>
-            Go to page
-            <input
-              type="number"
-              defaultValue={pageIndex + 1}
-              onChange={(e) => {
-                const page = e.target.value ? Number(e.target.value) - 1 : 0;
-                gotoPage(page);
-              }}
-              style={{ marginLeft: 8, width: "40px" }}
-            />
-          </span>
-        </div>
+        <Pagination
+          {...{
+            gotoPage,
+            previousPage,
+            nextPage,
+            canPreviousPage,
+            canNextPage,
+            pageCount,
+            pageIndex,
+            pageOptions,
+            pageSize,
+            setPageSize,
+          }}
+        />
       )}
     </TableStyles>
   );
@@ -301,49 +143,6 @@ const TableStyles = styled.div`
     overflow-x: auto;
     overflow-y: hidden;
     border-bottom: 1px solid rgba(224, 224, 224, 1);
-  }
-
-  .pagination {
-    display: flex;
-    align-items: center;
-    padding: 16px;
-    .indexItem {
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      font-family: "Spoqa Han Sans Neo", "sans-serif";
-      font-size: 14px;
-      width: 24px;
-      height: 24px;
-      background: transparent;
-      border: none;
-      padding: 0;
-      margin: 0 2px;
-      border-radius: 50%;
-      cursor: pointer;
-      user-select: none;
-    }
-    .arrowBtn {
-      border: none;
-      background: transparent;
-      cursor: pointer;
-      user-select: none;
-    }
-  }
-
-  .indexBtnGroup {
-    display: flex;
-    align-items: center;
-  }
-
-  .selectedPageIndex {
-    background: rgba(0, 0, 0, 0.6) !important;
-    color: #fff;
-    font-weight: 500;
-    border-radius: 50%;
-    margin: 0 2px;
-    user-select: none;
-    cursor: auto !important;
   }
 
   table {
