b1d8ed19c8b1f35c1edb79d840220b66c268ad1a
injulme <injulme0309@naver.com>
2022-12-10 16:19:24 +0900

    recoil setting(member)

---
+++ states/memberAtom.js
@@ -0,0 +1,14 @@
+import { atom } from "recoil";
+
+export const myNameState = atom({
+  key: "myNameState",
+  default: "hyojin",
+});
+
+export const myNameSelector = selector({
+  key: "myNameSelector",
+  get: ({ get }) => {
+    const data = get(myNameState);
+    return data;
+  },
+});
