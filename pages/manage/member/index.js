49d7824fb4d2fd55b79348d5596ad945dad5a7c5
injulme <injulme0309@naver.com>
2022-12-15 00:34:13 +0900

    rightSheet컴포넌트 적용 및 경/비 이력조회 페이지 구현중

--- pages/manage/member/index.js
+++ pages/manage/member/index.js
@@ -1,11 +1,47 @@
+import { useTheme } from "@emotion/react";
+import styled from "@emotion/styled";
+import Button from "components/common/Button";
 import Layout from "components/common/Layout";
-import { useRecoilState, useRecoilValue, useSetRecoilState } from "recoil";
-import { myNameState } from "states/memberAtom";
+import RightSheet from "components/common/RightSheet";
+import { useState } from "react";
 
 export default function MemberManagementPage() {
-  // const [myName, setMyName] = useRecoilState(myNameState);
-  // const setMyName = useSetRecoilState(myNameState)
-  const myName = useRecoilValue(myNameState);
+  const theme = useTheme();
+  const [showHistory, setShowHistory] = useState(false);
+  const onHistoryOpen = () => setShowHistory(true);
+  const onHistoryClose = () => setShowHistory(false);
+  return (
+    <Layout>
+      <button onClick={onHistoryOpen}>경고 및 비활성화 이력 조회</button>
+      {showHistory && (
+        <RightSheet visible={showHistory}>
+          <TitleBox theme={theme}>경고 및 비활성화 이력 조회</TitleBox>
 
-  return <Layout>여기는 {myName} 회원 관리</Layout>;
+          <ButtonBox>
+            <Button type={"secondary"} size={"md"} onClick={onHistoryClose}>
+              닫기
+            </Button>
+          </ButtonBox>
+        </RightSheet>
+      )}
+    </Layout>
+  );
 }
+
+const TitleBox = styled.div`
+  ${(p) => p.theme.typography.body1}
+  ${(p) => p.theme.typography.weightBold}
+  
+  color: ${(p) => p.theme.palette.colors.basic.black};
+  margin-bottom: 62px;
+
+  text-align: left;
+`;
+const ButtonBox = styled.div`
+  gap: 14px;
+  display: flex;
+  justify-content: center;
+  position: absolute;
+  bottom: 40px;
+  width: calc(100% - 200px);
+`;
