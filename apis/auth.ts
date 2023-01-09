fc9de571eeb6e241b56b2dd1eff728c61310333a
suhotang <dkdudkc22@gmail.com>
2023-01-10 00:21:12 +0900

    Refactor     - 사용자, 관리자 웹 분리

--- apis/auth.ts
+++ apis/auth.ts
@@ -1,22 +1,8 @@
 import axios from "axios";
 
-type getJwtTokenBody = {
-  provider: "kakao" | "naver" | "google";
-  token: string;
-};
-
 type managerLoginBody = {
   id: string;
   password: string;
-};
-
-export const getJwtToken = async (body: getJwtTokenBody) => {
-  try {
-    const response = await axios.post("/api/auth/token", body);
-    return response?.data;
-  } catch (error) {
-    throw error;
-  }
 };
 
 export const managerLogin = async (body: managerLoginBody) => {
--- pages/manage/index.tsx
+++
@@ -1,9 +0,0 @@
-import Layout from "components/common/Layout";
-
-export default function ManagementPage() {
-  return (
-    <Layout>
-      <div>관리자 메인 페이지</div>
-    </Layout>
-  );
-}
