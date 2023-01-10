46e0ec5a4eea00f7fa8321f145dabbb994d524dd
suhotang <dkdudkc22@gmail.com>
2023-01-10 17:16:31 +0900

    Fix     - react-perfect-scrollbar 추가     - RightSheet 컴포넌트 수정         - 하단에 닫기 버튼 넣을 수 있는 옵션 추가         - perfectScrollbar 적용         - padding, fontSize 수정 (사이즈 다운)

--- components/common/RightSheet.tsx
+++ components/common/RightSheet.tsx
@@ -1,26 +1,37 @@
+import { MouseEvent, PropsWithChildren } from "react";
 import { keyframes, useTheme } from "@emotion/react";
 import styled from "@emotion/styled";
-import { PropsWithChildren } from "react";
+import Button from "components/common/Button";
+import PerfectScrollbar from "react-perfect-scrollbar";
 
 type RightSheetPropsType = {
   visible?: boolean;
   title?: string;
+  onClose?: (e: MouseEvent<HTMLButtonElement>) => any;
 };
 
 export default function RightSheet({
   children,
   visible,
   title,
+  onClose,
 }: PropsWithChildren<RightSheetPropsType>) {
   const theme = useTheme();
 
   return (
     <Overlay>
       <StyledPopup visible={visible} theme={theme}>
-        <Container theme={theme}>
-          {title && <div className="title">{title}</div>}
-          {children}
-        </Container>
+        {title && <TitleBox>{title}</TitleBox>}
+        <PerfectScrollbar>
+          <Content theme={theme}>{children}</Content>
+        </PerfectScrollbar>
+        {typeof onClose === "function" && (
+          <ButtonBox>
+            <Button type="secondary" size="md" onClick={onClose}>
+              닫기
+            </Button>
+          </ButtonBox>
+        )}
       </StyledPopup>
     </Overlay>
   );
@@ -35,67 +46,71 @@ const Overlay = styled.div`
   overflow: hidden;
   touch-action: none;
   z-index: 10;
-
   background: rgba(0, 0, 0, 0.4);
 `;
 
-const Container = styled.div`
-  padding: 40px 100px;
+const TitleBox = styled.div`
+  ${(p) => p.theme.typography.body2}
+  ${(p) => p.theme.typography.weightBold}
+  
+  color: ${(p) => p.theme.palette.colors.basic.black};
+  text-align: left;
+  padding: 40px 32px;
+`;
 
-  .title {
-    ${(p) => p.theme.typography.body1}
-    ${(p) => p.theme.typography.weightBold}
-    
-    color: ${(p) => p.theme.palette.colors.basic.black};
-    margin-bottom: 62px;
+const Content = styled.div`
+  padding: 0px;
+  width: 100%;
+  //flex: 1;
+  padding: 0 32px;
+  box-sizing: border-box;
+  overflow-y: auto;
+`;
 
-    text-align: left;
-  }
+const ButtonBox = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  box-sizing: border-box;
+  padding: 24px 32px 40px;
 `;
 
 const StyledPopup = styled.div<{ visible: boolean }>`
   position: absolute;
   top: 0;
   right: 0;
-
   width: 800px;
   height: 100%;
-
   background: ${(p) => p.theme.palette.colors.basic.white};
   animation: ${(p) => (p.visible ? fadeInLeft : fadeInRight)} 1s;
 
   box-shadow: 0 0 0 100vw rgba(0, 0, 0, 0.5);
   border-radius: 48px 0px 0px 48px;
-`;
 
-const TitleBox = styled.div`
-  ${(p) => p.theme.typography.body1}
-  ${(p) => p.theme.typography.weightBold}
-  
-  color: ${(p) => p.theme.palette.colors.basic.black};
-  margin-bottom: 62px;
-
-  text-align: left;
+  padding: 0 32px;
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
 `;
 
 const fadeInLeft = keyframes`
-from {
-  opacity: 0;
-  transform: translate3d(100%, 0, 0);
-}
-to {
-  opacity: 1;
-  transform: translateZ(0);
-}
+  from {
+    opacity: 0;
+    transform: translate3d(100%, 0, 0);
+  }
+  to {
+    opacity: 1;
+    transform: translateZ(0);
+  }
 `;
 
 const fadeInRight = keyframes`
-from {
-  opacity: 1;
-  transform: translateZ(0);
-}
-to {
-  opacity: 0;
-  transform: translate3d(100%, 0, 0);
-}
+  from {
+    opacity: 1;
+    transform: translateZ(0);
+  }
+  to {
+    opacity: 0;
+    transform: translate3d(100%, 0, 0);
+  }
 `;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,4 @@
+import "react-perfect-scrollbar/dist/css/styles.css";
 import "styles/reset.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
