a6df21dc0dfa627623b5046559d7f52cac24805f
suhotang <dkdudkc22@gmail.com>
2023-01-21 16:15:03 +0900

    Feat(Dropdown 컴포넌트)     - dropdown input focus 상태 스타일 구현     - useOnClickOutside 적용     - 선택창 열렸을 때 up, 닫혔을 때 down 아이콘 표시     - 리스트에서 아이템 선택하면 선택창 닫히고 InputBox에 선택한 값 표시되도록 함 TODO     - OptionListContainer에 transition 효과 넣기

--- components/common/Dropdown.tsx
+++ components/common/Dropdown.tsx
@@ -1,15 +1,21 @@
-import { ChangeEvent, ReactElement, useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import styled from "@emotion/styled";
-import theme from "styles/theme";
-import { LabelField } from "./Input";
 import { createPopper } from "@popperjs/core";
+import theme from "styles/theme";
+import Icon from "./Icon";
+import useOnClickOutside from "utils/useOnClickOutside";
+
+type OptionType = {
+  id: string | number;
+  name: string;
+};
 
 type DropdownTypes = {
   id?: string;
   className?: string;
-  value?: number | string;
-  options: Array<{ id?: string | number; name: string }>;
-  onChange: (e: ChangeEvent<HTMLInputElement>) => any;
+  defaultValueId?: number | string;
+  options: Array<OptionType>;
+  onChange: (option: OptionType) => void;
   placeholder?: string;
   width?: string | number;
   height?: string | number;
@@ -20,7 +26,7 @@ type DropdownTypes = {
 export default function Dropdown({
   id,
   className,
-  value,
+  defaultValueId,
   options,
   onChange,
   placeholder = "필드를 입력해주세요.",
@@ -29,8 +35,11 @@ export default function Dropdown({
   label,
   disabled = false,
 }: DropdownTypes) {
+  const clickOutsideRef = useRef();
   const inputRef = useRef();
   const selectListRef = useRef();
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedId, setSelectedId] = useState(defaultValueId);
 
   useEffect(() => {
     if (inputRef && selectListRef) {
@@ -48,44 +57,130 @@ export default function Dropdown({
     }
   }, [inputRef, selectListRef]);
 
+  useOnClickOutside(clickOutsideRef, () => setIsOpen(false));
+
   return (
-    <div style={{ position: "relative", width }}>
-      <LabelField disabled={disabled}>
-        {label && <div className="label">{label}</div>}
+    <div className="custom-dropdown-input">
+      {label && <Label className="label">{label}</Label>}
+      <div ref={clickOutsideRef} style={{ position: "relative", width }}>
         <StyledInput
           id={id}
           className={className}
           ref={inputRef}
           placeholder={placeholder}
-          style={{ width, height }}
+          width={width}
+          height={height}
+          onClick={() => {
+            setIsOpen((value) => !value);
+          }}
         >
-          {value ?? placeholder}
+          {selectedId ? (
+            <span className="selected-item">
+              {options.filter((item) => item.id === selectedId)[0]?.name}
+            </span>
+          ) : (
+            <span className="placeholder">{placeholder}</span>
+          )}
+          <Icon icon={isOpen ? "up" : "down"} size="sm" />
         </StyledInput>
-      </LabelField>
-      <OptionListContainer ref={selectListRef} width={width}>
-        {options.map((option) => {
-          return <div className="option-item">{option.name}</div>;
-        })}
-      </OptionListContainer>
+        <OptionListContainer ref={selectListRef} width={width} visible={isOpen}>
+          <ul>
+            {options.map((option) => {
+              const isSelected = option.id === selectedId;
+              return (
+                <li
+                  className={
+                    isSelected ? "selected-option-item" : "option-item"
+                  }
+                  onClick={() => {
+                    onChange(option);
+                    setSelectedId(option.id);
+                    setIsOpen(false);
+                  }}
+                >
+                  {option.name}
+                </li>
+              );
+            })}
+          </ul>
+        </OptionListContainer>
+      </div>
     </div>
   );
 }
 
-const OptionListContainer = styled.div<{ width: string | number }>`
+const OptionListContainer = styled.div<{
+  width: string | number;
+  visible: boolean;
+}>`
+  ${theme.typography.body3}
+  ${theme.typography.weightMedium}
+
+  @keyframes growOut {
+    0% {
+      transform: scale(0);
+    }
+    80% {
+      transform: scale(1.1);
+    }
+    100% {
+      transform: scale(1);
+    }
+  }
+
   display: flex;
   flex-direction: column;
   background: #ffffff;
-  color: #643045;
-  font-weight: bold;
-  padding: 10px 16px;
-  font-size: 13px;
   border-radius: 4px;
-  border: 1px solid #000;
+  border: 1px solid ${theme.palette.colors.gray[500]};
   box-sizing: border-box;
   width: ${(p) => p.width};
+  opacity: ${(p) => (p.visible ? 1 : 0)};
+  animation: growOut 300ms ease-in-out forwards;
+
+  ul {
+    padding: 0;
+    margin: 0;
+    & > li:first-child {
+      border-radius: 4px 4px 0 0;
+    }
+    & > li:last-child {
+      border-radius: 0 0 4px 4px;
+    }
+
+    li {
+      background: #fff;
+      padding: 12px 20px;
+      cursor: pointer;
+      user-select: none;
+      &:hover {
+        color: ${theme.palette.colors.primary[500]};
+        background: ${theme.palette.colors.primary[100]};
+      }
+    }
+
+    .selected-option-item {
+      color: ${theme.palette.colors.primary[500]};
+      background: ${theme.palette.colors.primary[100]};
+    }
+  }
 `;
 
-const StyledInput = styled.div`
+const Label = styled.div<{ disabled?: boolean }>`
+  ${theme.typography.body3}
+  ${theme.typography.weightMedium}
+  display: inline-block;
+  color: ${(p) =>
+    p.disabled
+      ? theme.palette.colors.gray[300]
+      : theme.palette.colors.basic.black};
+  margin-bottom: 8px;
+`;
+
+const StyledInput = styled.div<{
+  width?: string | number;
+  height?: string | number;
+}>`
   ${theme.typography.body3}
   ${theme.typography.weightRegular}
 
@@ -94,24 +189,34 @@ const StyledInput = styled.div`
   -moz-appearance: none;
 
   display: flex;
+  justify-content: space-between;
   align-items: center;
   padding: 10px 16px;
   border: 1px solid ${theme.palette.colors.gray[300]};
   border-radius: 5px;
   color: ${theme.palette.colors.basic.black};
   box-sizing: border-box;
+  width: ${(p) => p.width};
+  height: ${(p) => p.height};
+  cursor: pointer;
+  user-select: none;
 
-  ::placeholder {
+  &:hover {
+    border: 1px solid ${theme.palette.colors.gray[500]};
+  }
+
+  .placeholder {
     color: ${theme.palette.colors.gray[400]};
   }
-  &:focus {
-    border: 1px solid ${theme.palette.colors.gray[500]};
-    outline: none;
-  }
 
-  :disabled {
+  .disabled {
     background-color: ${theme.palette.colors.gray[50]};
     border: 1px solid ${theme.palette.colors.gray[200]};
     color: ${theme.palette.colors.gray[400]};
   }
+
+  /* &:focus {
+    border: 1px solid ${theme.palette.colors.gray[500]};
+    outline: none;
+  } */
 `;
