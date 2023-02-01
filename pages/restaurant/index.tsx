3bd12c4f978a2e4e5acbd9a39f92bee0d495b145
suhotang <dkdudkc22@gmail.com>
2023-02-01 23:46:51 +0900

    Feat(채식 식당)   - 채식 식당 탭뷰 구현 TODO   - 신청 검수 현황 테이블 내용 정의   - 오른쪽 상세 정보 조회 뷰 구현

--- pages/restaurant/index.tsx
+++ pages/restaurant/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import range from "utils/range";
 import styled from "@emotion/styled";
 import usePopup from "utils/usePopup";
@@ -15,15 +15,80 @@ const ToolbarWrapper = styled.div`
 
 type PopupType = "create" | "detail";
 
-export default function RestaurantPage() {
-  const { popupOption, onPopupOpen, onPopupClose } = usePopup<PopupType>();
-  const [selectedTab, setSelectedTab] = useState(0);
-  //const [data, setData] = useState([]);
-  const columns = getTableColumns(selectedTab, onPopupOpen);
+const TableTabs = {
+  register_status: 0, // 등록 현황
+  inspection_status: 1, // 신청 현황
+};
 
-  const data = range(50).map((index) => {
+const TabViewStyle = styled.div`
+  .tabbar {
+    display: flex;
+    .tabbar-item {
+      padding: 12px 16px;
+      cursor: pointer;
+      user-select: none;
+    }
+
+    .selected-item {
+      border-bottom: 3px solid #ff744d;
+    }
+
+    & > .tabbar-item:first-child {
+      border-right: none;
+    }
+  }
+
+  .tab-contents {
+    padding: 20px;
+  }
+`;
+
+const TabView = ({ tabList, selectedTabId, contents, onTabChanegd }) => {
+  return (
+    <TabViewStyle>
+      <div className="tabbar">
+        {tabList.map((item) => {
+          const isSelected = selectedTabId === item?.id;
+          return (
+            <div
+              className={`tabbar-item ${isSelected ? "selected-item" : ""}`}
+              onClick={() => onTabChanegd(item.id)}
+            >
+              {item.name}
+            </div>
+          );
+        })}
+      </div>
+      <div className="tab-contents">{contents}</div>
+    </TabViewStyle>
+  );
+};
+
+export default function RestaurantPage(/*{ data: serverData }*/) {
+  const { popupOption, onPopupOpen, onPopupClose } = usePopup<PopupType>();
+  const [selectedTab, setSelectedTab] = useState(TableTabs?.register_status);
+  const registerStatusColumns = getTableColumns(
+    TableTabs?.register_status,
+    onPopupOpen
+  );
+  const registerStatusList = range(50).map((id) => {
     return {
-      name: `상호명-${index}`,
+      name: `등록 현황 상호명-${id}`,
+      vegetarianType: "폴로",
+      createdAt: "2023-01-12",
+      category: "한식",
+      multipleSelect: "채식옵션",
+      request: "김뫄뫄",
+      manager: "관리자",
+    };
+  });
+  const inspectionStatusColumns = getTableColumns(
+    TableTabs?.inspection_status,
+    onPopupOpen
+  );
+  const inspectionStatusList = range(50).map((id) => {
+    return {
+      name: `검수 현황 상호명-${id}`,
       vegetarianType: "폴로",
       createdAt: "2023-01-12",
       category: "한식",
@@ -33,10 +98,57 @@ export default function RestaurantPage() {
     };
   });
 
+  const TabContents = () => {
+    return (
+      <Table
+        id="restuarant-management-table"
+        columns={
+          selectedTab === TableTabs?.register_status
+            ? registerStatusColumns
+            : inspectionStatusColumns
+        }
+        data={
+          selectedTab === TableTabs?.register_status
+            ? registerStatusList
+            : inspectionStatusList
+        }
+      />
+    );
+  };
+
   return (
     <Layout>
       <ToolbarWrapper></ToolbarWrapper>
-      <Table id="restuarant-management-table" columns={columns} data={data} />
+      <TabView
+        selectedTabId={selectedTab}
+        tabList={[
+          { id: TableTabs.register_status, name: "등록 현황" },
+          { id: TableTabs?.inspection_status, name: "신청 검수 현황" },
+        ]}
+        contents={<TabContents />}
+        onTabChanegd={(tabId) => {
+          setSelectedTab(tabId);
+        }}
+      />
     </Layout>
   );
 }
+
+// export function getServerSideProps(_context) {
+//   const res = range(50).map((id) => {
+//     return {
+//       name: `등록현황 상호명-${id}`,
+//       vegetarianType: "폴로",
+//       createdAt: "2023-01-12",
+//       category: "한식",
+//       multipleSelect: "채식옵션",
+//       request: "김뫄뫄",
+//       manager: "관리자",
+//     };
+//   });
+//   const data = JSON.stringify(res);
+
+//   return {
+//     props: { data }, // will be passed to the page component as props
+//   };
+// }
